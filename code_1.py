401c809a90a41269ebd0c51bfd2153d5c39b0173
segabaser <139810447+segabaser@users.noreply.github.com>
2023-09-22 01:00:06 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,42 @@
+function main() {
+
+
+}
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+    data.forEach(item => {
+    });
+        console.log("Random Number: " + item);
+    }
+    const data = [];
+    return data;
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+
+    public static void main(String[] args) {
+
+    }
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+}
+        }
+public class RandomDataGenerator {
+        for (int item : data) {    return data
+def main():
+
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+if __name__ == "__main__":
+import random
+
+    data = generate_random_data()
+
+    for item in data:
+    main()
